155abe1e7e230aca3126268c757a8b2a97fb323a
dmitriy <sumin_1993@mail.ru>
2018-02-24 13:44:31 +0700

    Добавил функцию объединения интервалов

--- Range/src/Main.java
+++ Range/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(1, 3);
-        Range range1 = new Range(6, 7);
+        Range range = new Range(1, 4);
+        Range range1 = new Range(3, 20);
         System.out.println("Число входит в заданный диапазон: " + range.isInside(1));
         System.out.println("длина диапазона: " + range.getLength());
-        System.out.println(range.getIntersection(range1));
+        Object[] array = range.getAssociation(range1);
+        for (Object e : array) {
+            System.out.println(e);
+        }
     }
 }
--- Range/src/Range.java
+++ Range/src/Range.java
@@ -9,16 +9,41 @@ public class Range {
     }
 
     public Range getIntersection(Range range) {
-        if ((range.getFrom() > from) && (range.getTo() < to)) {
-            return new Range(range.getFrom(), range.getTo());
-        } else if ((from > range.getFrom()) && (to < range.getTo())) {
-            return new Range(from, to);
-        } else if ((from < range.getFrom()) && (range.getFrom() < to)) {
-            return new Range(range.getFrom(), to);
-        } else if ((range.getFrom() < from) && (from < range.getTo())) {
-            return new Range(range.getTo(), to);
-        } else {
+        if ((from < range.getFrom() && (to < range.getFrom()) ||(range.getFrom() < from) && (range.getTo() < from))){
             return null;
+        } else if ((range.getFrom() > from) && (range.getTo() < to)){
+            return new Range(range.getFrom(), range.getTo());
+        } else if ((from > range.getFrom()) && (to < range.getTo())){
+            return new Range(from, to);
+        } else if((from < range.getFrom()) && (range.getFrom() < to)){
+            return new Range(range.getFrom(), to);
+        } else {
+            return new Range(from, range.getTo());
+        }
+    }
+
+    public Object [] getAssociation(Range range){
+        Object [] arrayRange = new Object[2];
+        if ((from < range.getFrom() && (to < range.getFrom()) ||(range.getFrom() < from) && (range.getTo() < from))){
+            arrayRange[0] = new Range(from, to);
+            arrayRange[1] = new Range(range.getFrom(), range.getTo());
+            return arrayRange;
+        } else if ((range.getFrom() > from) && (range.getTo() < to)){
+            arrayRange[0] = new Range(from, to);
+            arrayRange[1] = null;
+            return arrayRange;
+        } else if ((from > range.getFrom()) && (to < range.getTo())){
+            arrayRange[0] = new Range(range.getFrom(), range.getTo());
+            arrayRange[1] = null;
+            return arrayRange;
+        } else if((from < range.getFrom()) && (range.getFrom() < to)){
+            arrayRange[0] = new Range(from, range.getTo());
+            arrayRange[1] = null;
+            return arrayRange;
+        } else {
+            arrayRange[0] = new Range(range.getFrom(), to);
+            arrayRange[1] = null;
+            return arrayRange;
         }
     }
 

21a791a55b151441e419ca5e37380631f479c32d
dmitriy <sumin_1993@mail.ru>
2018-02-25 20:26:56 +0700

    Добавил вычитание интервалов

--- Range/src/Main.java
+++ Range/src/Main.java
@@ -2,13 +2,13 @@ import ru.academits.Range;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(2, 10);
-        Range range1 = new Range(1, 15);
+        Range range = new Range(5, 8);
+        Range range1 = new Range(1, 10);
         System.out.println("Число входит в заданный диапазон: " + range.isInside(1));
         System.out.println("длина диапазона: " + range.getLength());
         //System.out.println(range.getIntersection(range1));
-        Object[] array = range.getUnionRange(range1);
-        for (Object e : array) {
+        Range[] array = range.getDifferenceRange(range1);
+        for (Range e : array) {
             System.out.println(e);
         }
     }
--- Range/src/ru/academits/Range.java
+++ Range/src/ru/academits/Range.java
@@ -11,16 +11,16 @@ public class Range {
     }
 
     public Range getIntersection(Range range) {
-        if ((from < range.from && (to < range.from) || (range.from < from) && (range.to < from))) {
+        if ((from < range.from && (to < range.from) || (range.from < from) && (range.to < from) || (to == range.from) || (range.to == from))) {
             return null;
-        } else if ((range.from > from) && (range.to < to)) {
+        } else if ((range.from >= from) && (range.to <= to)) {
             return new Range(range.from, range.to);
-        } else if ((from > range.from) && (to < range.to)) {
+        } else if ((from >= range.from) && (to <= range.to)) {
             return new Range(from, to);
         } else if ((from < range.from) && (range.from < to)) {
             return new Range(range.from, to);
         } else {
-            return new Range(from, to);
+            return new Range(from, range.to);
         }
     }
 
@@ -30,15 +30,15 @@ public class Range {
             range1[0] = new Range(from, to);
             range1[1] = new Range(range.from, range.to);
             return range1;
-        } else if ((range.from > from) && (range.to < to)) {
+        } else if ((range.from >= from) && (range.to <= to)) {
             Range[] range1 = new Range[1];
             range1[0] = new Range(from, to);
             return range1;
-        } else if ((from > range.from) && (to < range.to)) {
+        } else if ((from >= range.from) && (to <= range.to)) {
             Range[] range1 = new Range[1];
             range1[0] = new Range(range.from, range.to);
             return range1;
-        } else if ((from < range.from) && (range.from < to)) {
+        } else if ((from < range.from) && (range.from <= to)) {
             Range[] range1 = new Range[1];
             range1[0] = new Range(from, range.to);
             return range1;
@@ -50,41 +50,56 @@ public class Range {
     }
 
     public Range[] getDifferenceRange(Range range) {
-        if ((from < range.from) && (to < range.from)) {
+        if (((from < range.from) && (to < range.from)) || ((range.from < from) && (range.to < from))) {
             Range[] range1 = new Range[2];
             range1[0] = new Range(from, to);
             range1[1] = new Range(range.from, range.to);
             return range1;
-        } else if ((range.from < from) && (range.to < from)) {
-            Range[] range1 = new Range[2];
-            range1[0] = new Range(range.from, range.to);
-            range1[1] = new Range(from, to);
-            return range1;
         } else if ((range.from < from) && (range.to > to)) {
             Range[] range1 = new Range[2];
             range1[0] = new Range(range.from, from);
-            range1[0] = new Range(to, range.to);
+            range1[1] = new Range(to, range.to);
             return range1;
         } else if (((from < range.from) && (to > range.to))) {
             Range[] range1 = new Range[2];
             range1[0] = new Range(from, range.from);
-            range1[1] = new Range(range.from, to);
+            range1[1] = new Range(range.to, to);
+            return range1;
+        } else if ((from == range.from) && ((to > range.to) || (range.to > to))) {
+            Range[] range1 = new Range[1];
+            if (to > range.to) {
+                range1[0] = new Range(range.to, to);
+            } else {
+                range1[0] = new Range(to, range.to);
+            }
+            return range1;
+        } else if (((from > range.from) || (from < range.from)) && (range.to == to)) {
+            Range[] range1 = new Range[1];
+            if (from < range.from) {
+                range1[0] = new Range(from, range.from);
+            } else {
+                range1[0] = new Range(range.from, from);
+            }
             return range1;
         } else if (((from < range.from) && (to > range.from))) {
             Range[] range1 = new Range[2];
             range1[0] = new Range(from, range.from);
             range1[1] = new Range(to, range.to);
             return range1;
-        } else {
+        } else if ((range.from < from) && (range.to < to)) {
             Range[] range1 = new Range[2];
             range1[0] = new Range(range.from, from);
             range1[1] = new Range(range.to, to);
+            return range1;
+        } else {
+            Range[] range1 = new Range[1];
+            range1[0] = null;
             return range1;
         }
     }
 
     public String toString() {
-        return "Начальный элемент диапазона:" + from + " конечный элемент диапазона: " + to;
+        return "Начальный элемент диапазона: " + from + " конечный элемент диапазона: " + to;
     }
 
     public double getFrom() {

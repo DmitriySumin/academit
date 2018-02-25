986f9e4fc172a45fa14f5cbe84c0a6ffb65c4352
dmitriy <sumin_1993@mail.ru>
2018-02-25 12:25:59 +0700

    Практическое задание

--- Range/src/Main.java
+++ Range/src/Main.java
@@ -1,10 +1,13 @@
+import ru.academits.Range;
+
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(1, 4);
-        Range range1 = new Range(3, 20);
+        Range range = new Range(2, 10);
+        Range range1 = new Range(1, 15);
         System.out.println("Число входит в заданный диапазон: " + range.isInside(1));
         System.out.println("длина диапазона: " + range.getLength());
-        Object[] array = range.getAssociation(range1);
+        //System.out.println(range.getIntersection(range1));
+        Object[] array = range.getUnionRange(range1);
         for (Object e : array) {
             System.out.println(e);
         }

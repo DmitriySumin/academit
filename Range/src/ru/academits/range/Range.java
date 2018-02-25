4b47b19980dc31c2717d72818cb87fd06495df00
dmitriy <sumin_1993@mail.ru>
2018-02-25 21:24:34 +0700

    Доработка замечаний

--- Range/src/ru/academits/range/Range.java
+++ Range/src/ru/academits/range/Range.java
@@ -1,4 +1,4 @@
-package ru.academits;
+package ru.academits.range;
 
 public class Range {
 
@@ -11,7 +11,7 @@ public class Range {
     }
 
     public Range getIntersection(Range range) {
-        if ((from < range.from && (to < range.from) || (range.from < from) && (range.to < from) || (to == range.from) || (range.to == from))) {
+        if ((from < range.from) && (to <= range.from) || (range.from < from) && (range.to <= from)) {
             return null;
         } else if ((range.from >= from) && (range.to <= to)) {
             return new Range(range.from, range.to);
@@ -26,74 +26,47 @@ public class Range {
 
     public Range[] getUnionRange(Range range) {
         if ((from < range.from && (to < range.from) || (range.from < from) && (range.to < from))) {
-            Range[] range1 = new Range[2];
-            range1[0] = new Range(from, to);
-            range1[1] = new Range(range.from, range.to);
+            Range[] range1 = {new Range(from, to), new Range(range.from, range.to)};
             return range1;
         } else if ((range.from >= from) && (range.to <= to)) {
-            Range[] range1 = new Range[1];
-            range1[0] = new Range(from, to);
+            Range[] range1 = {new Range(from, to)};
             return range1;
         } else if ((from >= range.from) && (to <= range.to)) {
-            Range[] range1 = new Range[1];
-            range1[0] = new Range(range.from, range.to);
+            Range[] range1 = {new Range(range.from, range.to)};
             return range1;
         } else if ((from < range.from) && (range.from <= to)) {
-            Range[] range1 = new Range[1];
-            range1[0] = new Range(from, range.to);
+            Range[] range1 = {new Range(from, range.to)};
             return range1;
         } else {
-            Range[] range1 = new Range[1];
-            range1[0] = new Range(range.from, to);
+            Range[] range1 = {new Range(range.from, to)};
             return range1;
         }
     }
 
     public Range[] getDifferenceRange(Range range) {
         if (((from < range.from) && (to < range.from)) || ((range.from < from) && (range.to < from))) {
-            Range[] range1 = new Range[2];
-            range1[0] = new Range(from, to);
-            range1[1] = new Range(range.from, range.to);
+            Range[] range1 = {new Range(from, to), new Range(range.from, range.to)};
             return range1;
         } else if ((range.from < from) && (range.to > to)) {
-            Range[] range1 = new Range[2];
-            range1[0] = new Range(range.from, from);
-            range1[1] = new Range(to, range.to);
+            Range[] range1 = {new Range(range.from, from), new Range(to, range.to)};
             return range1;
         } else if (((from < range.from) && (to > range.to))) {
-            Range[] range1 = new Range[2];
-            range1[0] = new Range(from, range.from);
-            range1[1] = new Range(range.to, to);
-            return range1;
-        } else if ((from == range.from) && ((to > range.to) || (range.to > to))) {
-            Range[] range1 = new Range[1];
-            if (to > range.to) {
-                range1[0] = new Range(range.to, to);
-            } else {
-                range1[0] = new Range(to, range.to);
-            }
-            return range1;
-        } else if (((from > range.from) || (from < range.from)) && (range.to == to)) {
-            Range[] range1 = new Range[1];
-            if (from < range.from) {
-                range1[0] = new Range(from, range.from);
-            } else {
-                range1[0] = new Range(range.from, from);
-            }
+            Range[] range1 = {new Range(from, range.from), new Range(range.to, to)};
             return range1;
         } else if (((from < range.from) && (to > range.from))) {
-            Range[] range1 = new Range[2];
-            range1[0] = new Range(from, range.from);
-            range1[1] = new Range(to, range.to);
+            Range[] range1 = {new Range(from, range.from), new Range(to, range.to)};
+            return range1;
+        } else if ((from == range.from) && (to > range.to)) {
+            Range[] range1 = {new Range(range.to, to)};
+            return range1;
+        } else if ((to == range.to) && (from < range.from)) {
+            Range[] range1 = {new Range(from, range.from)};
             return range1;
         } else if ((range.from < from) && (range.to < to)) {
-            Range[] range1 = new Range[2];
-            range1[0] = new Range(range.from, from);
-            range1[1] = new Range(range.to, to);
+            Range[] range1 = {new Range(range.from, from), new Range(range.to, to)};
             return range1;
         } else {
-            Range[] range1 = new Range[1];
-            range1[0] = null;
+            Range[] range1 = {};
             return range1;
         }
     }
